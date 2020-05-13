cdcc8851226829c7977f5019e2a2ddda151dc36e
imuhammed <ishaqmuhammed191@gmail.com>
2020-05-13 16:04:33 +0100

    This an commit on a project titled "Lecturer evaluation system for FUTMinna,"

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from account.models import Profile
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin, User
 
-# Register your models here.
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    raw_id_fields = ['user']
+
+
+class UserAdmin(AuthUserAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+
+# unregister old user admin
+admin.site.unregister(User)
+# register new user admin
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Profile)
--- account/models.py
+++ account/models.py
@@ -2,12 +2,21 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from faculty.models import Department
 
 
 class Profile(models.Model):
+
+    DEPARTMENTS = (
+        ('cyber security science', 'Cyber Security Science'),
+        ('computer science', 'Computer Science'),
+        ('information and media technology', 'Information and Media Technology'),
+    )
+
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    department = models.DateField(blank=True, null=True)
-    level = models.IntegerField(blank=True)
+    # department = models.CharField(max_length=40, choices=DEPARTMENTS, null=True, blank=True)
+    department = models.OneToOneField(Department, on_delete=models.CASCADE, null=True, blank=True)
+    level = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return f'profile for user {self.user.username}'
+        return f'{self.user.username}'
