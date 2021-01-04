c3341fd7aaa6da01a3e3d342baef369958db7c24
imuhammed24 <ishaqmuhammed011@gmail.com>
2021-01-04 13:07:54 +0100

    Evaluation forms are now successfully saved to database from templates

--- assessment/urls.py
+++ assessment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import evaluate_view
+from .views import evaluate_view, add_answers_view
 from lEvaluation.views import index
 
 urlpatterns = [
+    path('add-answer/<int:assessment_id>/', add_answers_view, name='add_answers'),
     path('evaluate/<int:staff_id>/<int:course_id>/<int:semester_id>/', evaluate_view, name='evaluate'),
-
+    # path('add-answer/', add_answers_view, 'add_answers'),
 ]
--- assessment/views.py
+++ assessment/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from assessment.models import Semester, Assessment
+from account.models import AssessedCourses
+from assessment.models import Semester, Assessment, Question, Answer
 from faculty.models import Course, CurrentSemester
 
 
@@ -16,3 +18,19 @@ def evaluate_view(request, staff_id, course_id, semester_id):
         'assessment': assessment,
     }
     return render(request, 'assessment/assessment.html', context)
+
+
+def add_answers_view(request, assessment_id):
+    assessment = Assessment.objects.get(id=assessment_id)
+    if request.method == 'POST':
+        for count, item in enumerate(request.POST):
+            if count > 0:
+                question_id = item
+                question = Question.objects.get(id=question_id, exam=assessment)
+                answer = Answer(text=request.POST.get(item), question=question, student=request.user)
+                answer.save()
+                confirm_assessment = AssessedCourses(user=request.user, assessment=assessment)
+                confirm_assessment.save()
+            if count > 14:
+                messages.success(request, "Response Added Successfully!")
+    return redirect('account:home')
